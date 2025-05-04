bc0539413b969cd67db318162ce6fc903b5fdb1e
iatharva6119 <datharva919@gmail.com>
2025-05-04 11:48:10 +0530

    first commit

---
+++ src/api/config.ts
@@ -0,0 +1,9 @@
+export const API_CONFIG = {
+  BASE_URL: "https://api.openweathermap.org/data/2.5",
+  GEO: "https://api.openweathermap.org/geo/1.0",
+  API_KEY: "639d84b48fe5eea038693a9577367852",
+  DEFAULT_PARAMS: {
+    units: "metric",
+    appid: "639d84b48fe5eea038693a9577367852",
+  },
+};
